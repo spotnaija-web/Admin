7296dfd46fe9e53757df33b378519c8592d8459b
Jed Ogwumike <jedhppc@gmail.com>
2024-06-04 15:53:50 +0100

    Dashboard update

--- src/apis/apiconfig.js
+++ src/apis/apiconfig.js
@@ -1,4 +1,6 @@
 // let baseurl = "http://3.213.159.44:3000"
-const baseurl = "https://proxy-td44.onrender.com"
-
-export { baseurl }
+// const baseurl = "https://proxy-td44.onrender.com"
+const baseurl = "https://spotn-backend.onrender.com"
+export {
+    baseurl
+}
