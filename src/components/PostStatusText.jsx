3b3d7610786536d608b68725a51a09314106dda2
Jed Ogwumike <jedhppc@gmail.com>
2024-07-05 04:53:06 +0100

    Fix posts view and edit page

--- src/components/PostStatusText.jsx
+++ src/components/PostStatusText.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const PostStatusText = ({ publishStatus }) => {
   const statusText = publishStatus ? 'Published' : 'Not Yet Approved';
-  const statusColor = publishStatus ? 'green' : 'red';
+  const statusColor = publishStatus ? "text-[#2ea103]" : "text-[#d7391e]";
 
   return (
-    <p className={`text-sm font-bold bg-white p-2 rounded text-${statusColor}-500`}>
+    <p className={`text-sm font-bold bg-white p-2 rounded ${statusColor}`}>
       {statusText}
     </p>
   );
